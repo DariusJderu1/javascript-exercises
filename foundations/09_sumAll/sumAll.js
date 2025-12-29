69fba7b7b5e0fdeb24d7518f52ee5569af75c3b9
DariusJderu1 <jderudarius@gmail.com>
2025-12-29 13:37:44 +0200

    Add solution

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(n1, n2) {
 
+    if(n1 < 0 || n2 < 0 || !Number.isInteger(n1) || !Number.isInteger(n2))
+        return `ERROR`;
+
+    if(n1 > n2) {
+
+        let aux = n1;
+        n1 = n2;
+        n2 = aux;
+    }
+
+    let nr = n2 - n1 + 1;
+    return ((n1 + n2) * nr) / 2;
 };
 
 // Do not edit below this line
