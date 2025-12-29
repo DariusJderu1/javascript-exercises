6c44a30e917a660c902ad15efcaf89b35190839b
DariusJderu1 <jderudarius@gmail.com>
2025-12-29 13:29:23 +0200

    Finish exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...elementsToRemove) {
+
+    for(let el of elementsToRemove)
+        for(let i = 0; i < arr.length; ++i) {
+            
+            if(arr[i] === el) {
+
+                arr.splice(i,1);
+                --i;
+            }
+        }
+         
+    return arr;
 };
 
 // Do not edit below this line
