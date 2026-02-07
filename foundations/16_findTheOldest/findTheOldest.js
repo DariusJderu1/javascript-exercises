994532231ac83c4a3a4c7545b4980dc63597c21e
DariusJderu1 <jderudarius@gmail.com>
2026-02-07 23:43:26 +0200

    Finish

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    const getAge = function(birth, death) {
+
+        if(!death) 
+            death = new Date().getFullYear();
+    
+        return death - birth;
+    };
+
+    return people.reduce((oldest, curr) => {
+
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = getAge(curr.yearOfBirth, curr.yearOfDeath);
+
+        return currentAge > oldestAge ? curr : oldest;
+    }, people[0]);
 };
 
 // Do not edit below this line
