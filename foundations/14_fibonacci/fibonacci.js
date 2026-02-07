c65fd8a4b805f5eb963639da65cdea05c2d3b59f
DariusJderu1 <jderudarius@gmail.com>
2026-02-07 23:20:00 +0200

    Finish

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
 
+    let a = 1, b = 1, c;
+
+    n = parseInt(n);
+
+    if(n < 0)
+        return "OOPS";
+
+    if(n == 0)
+        return 0;
+
+    if(n === 1 || n === 2)
+        return a;
+
+    for(let i = 3; i <= n; ++i) {
+
+        c = a + b; 
+        a = b;
+        b = c;
+    }
+
+    return c;
 };
 
 // Do not edit below this line
